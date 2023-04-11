8cbce67802885601792ff21917d455ed00d9ed0f
Kislvlad <vlad0kislyak@gmail.com>
2023-04-11 22:33:24 +0300

    task 5 v1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, 1 };
+            int max = 0;
+            for(int i=1;i<nums.Length; i++)
+                if (nums[i] > nums[max])
+                    max = i;
+            Console.WriteLine(max);
+        }
+    }
+}
